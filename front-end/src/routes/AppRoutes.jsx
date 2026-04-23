2cd1827d1776fdf7f10d19a86d18f721d43f1302
Jalisson Junior <jalisson.brito@fatec.sp.gov.br>
2026-04-23 14:22:50 +0000

    (23/04) Início da implementação de autenticação no front-end

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -5,13 +5,17 @@ import Homepage from '../pages/Homepage'
 import CarsList from '../pages/cars/CarsList'
 import CustomersList from '../pages/customers/CustomersList'
 
+import LoginPage from '../pages/LoginPage'
+
+
 export default function AppRoutes() {
- return <Routes>
-   <Route path="/" element={ <Homepage /> } />
+  return <Routes>
+    <Route path="/" element={<Homepage />} />
 
+    <Route path="/login" element={<LoginPage />} />
 
-   <Route path="/cars" element={ <CarsList /> } />
-   <Route path="/customers" element={ <CustomersList /> } />
+    <Route path="/cars" element={<CarsList />} />
+    <Route path="/customers" element={<CustomersList />} />
 
- </Routes>
+  </Routes>
 }
